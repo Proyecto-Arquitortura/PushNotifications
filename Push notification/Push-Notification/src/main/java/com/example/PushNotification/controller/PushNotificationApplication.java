b62302605e94ae98f954cf6d1ce4b5281ba9200e
santiagomenesses <72476704+santiagomenesses@users.noreply.github.com>
2022-04-18 10:19:38 -0500

    Commit de todo

---
+++ Push notification/Push-Notification/src/main/java/com/example/PushNotification/controller/PushNotificationApplication.java
@@ -0,0 +1,15 @@
+package com.example.PushNotification.controller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PushNotificationApplication {
+
+	public static void main(String[] args) {
+		
+		SpringApplication.run(PushNotificationApplication.class, args);
+	    System.out.println("push notification");
+	}
+
+}
